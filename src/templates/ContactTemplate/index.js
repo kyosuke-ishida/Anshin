17abe113bbbca8bef17e7546f65d2a7480e1cd2c
= <=>
2020-06-02 23:48:51 +0900

    Solved input error

--- src/templates/ContactTemplate/index.js
+++ src/templates/ContactTemplate/index.js
@@ -12,11 +12,11 @@ import {
 import img             from '../../../public/sampleimage.001.jpeg';
 
 const items = [
-    {term: 'お名前'                 , description: <span className={styles.inputwrapper}><input type="text"  name="name"        value="name"        label="お名前" 　　　　　　className={styles.input} /></span>},
-    {term: '法人名'                 , description: <span className={styles.inputwrapper}><input type="text"  name="corporation" value="corporation" label="法人名" 　　　　　　className={styles.input} /></span>},
-    {term: 'メールアドレス'           , description: <span className={styles.inputwrapper}><input type="email" name="email"       value="email"       label="メールアドレス" 　　className={styles.input} /></span>},
-    {term: '電話番号'                , description: <span className={styles.inputwrapper}><input type="text"  name="tel-num"     value="tel-num"     label="電話番号" 　　　　　className={styles.input} /></span>},
-    {term: 'お問い合わせ内容'         , description: <span className={styles.inputwrapper}><textarea           name="message"      value="message"     label="お問い合わせ内容"   className={styles.textarea}></textarea></span>},
+    {term: 'お名前'                 , description: <span className={styles.inputwrapper}><input type="text"  name="name"              label="お名前" 　　　　　　className={styles.input} /></span>},
+    {term: '法人名'                 , description: <span className={styles.inputwrapper}><input type="text"  name="corporation"       label="法人名" 　　　　　　className={styles.input} /></span>},
+    {term: 'メールアドレス'           , description: <span className={styles.inputwrapper}><input type="email" name="email"             label="メールアドレス" 　　className={styles.input} /></span>},
+    {term: '電話番号'                , description: <span className={styles.inputwrapper}><input type="text"  name="tel-num"          label="電話番号" 　　　　　className={styles.input} /></span>},
+    {term: 'お問い合わせ内容'         , description: <span className={styles.inputwrapper}><textarea           name="message"           label="お問い合わせ内容"   className={styles.textarea}></textarea></span>},
     {term: ''                       , description: <PrimaryButton type="submit">送信</PrimaryButton>}
 ];
 
